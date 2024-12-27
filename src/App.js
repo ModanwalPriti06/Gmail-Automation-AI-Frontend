3e2c0840a2cf27b5b0f2b8e9b1f4f9998361b941
Priti Modanwal <pritivns612@gmail.com>
2024-12-27 16:50:34 +0530

    git pusherf

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 
+import { useState } from 'react';
 import './App.css';
+import useDebounce from './useDebounce';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  const loadData = async (e)=>{
+    const value = e.target.value;
+    if(value === ''){
+      setData(null);
+      return;
+    } 
+    const response=await fetch(`http://localhost:3001/data/${value}`);
+    const res=await response.json()
+    console.log("response",res)
+    setData(res);
+  }
+  const loadDataDebounced = useDebounce(loadData, 400)
+
   return (
     <div className="App">
-      Hello World!...
+        <input type='text' placeholder = 'Enter Something' onChange={(e)=> loadDataDebounced(e)}/>
+        {data && data.length !== 0  &&
+          <div className='results-container'>
+            {data.map(item=>(
+              <div key={item.id} className="result-item">
+                 <p> {item.city} </p>
+              </div>
+            ))}
+          </div>
+        }
     </div>
   );
 }
